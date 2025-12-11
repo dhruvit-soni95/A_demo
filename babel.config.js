9fde7581de19b1815ad3daef5be8e3119c3b089f
dhruvit-soni95 <dhruvit.soni@paramsolutions.ca>
2025-12-11 10:23:43 +0530

    Remove worklets and fix Reanimated config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,43 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [
+    "react-native-reanimated/plugin",
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        alias: {
+          "@": "./",
         },
-      ],
-      'react-native-reanimated/plugin',
+      },
     ],
-  };
+  ],
 };
+
+
+
+
+
+
+
+
+
+
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: [
+//       [
+//         'module-resolver',
+//         {
+//           root: ['./'],
+//           alias: {
+//             '@': './',
+//           },
+//         },
+//       ],
+//       'react-native-reanimated/plugin',
+//     ],
+//   };
+// };
