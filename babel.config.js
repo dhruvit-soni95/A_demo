041bdbe122f00bb189bf314f5851ea053e6604ff
dhruvit-soni95 <dhruvit.soni@paramsolutions.ca>
2025-12-12 08:03:19 +0530

    fix: remove worklets & regenerate lockfile

--- babel.config.js
+++ babel.config.js
@@ -1,43 +1,43 @@
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: [
-    "react-native-reanimated/plugin",
-    [
-      "module-resolver",
-      {
-        root: ["./"],
-        alias: {
-          "@": "./",
-        },
-      },
-    ],
-  ],
-};
-
-
-
-
-
-
-
-
-
-
-// module.exports = function(api) {
-//   api.cache(true);
-//   return {
-//     presets: ['babel-preset-expo'],
-//     plugins: [
-//       [
-//         'module-resolver',
-//         {
-//           root: ['./'],
-//           alias: {
-//             '@': './',
-//           },
+// module.exports = {
+//   presets: ["babel-preset-expo"],
+//   plugins: [
+//     "react-native-reanimated/plugin",
+//     [
+//       "module-resolver",
+//       {
+//         root: ["./"],
+//         alias: {
+//           "@": "./",
 //         },
-//       ],
-//       'react-native-reanimated/plugin',
+//       },
 //     ],
-//   };
+//   ],
 // };
+
+
+
+
+
+
+
+
+
+
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
